cbbec64018aa70a4f0f29ade080351d141b1636d
valluru-yashwanth-reddy <valluru.yashwanthreddy@minfytech.com>
2025-09-10 15:27:13 +0530

    added token

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+yash = 'ASIA2XYQY5Y2SR2FBBWL';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
